b703656bd78ad47c905ca67d276da0418e416b70
MD. Asifur Rahman <imdjiban@gmail.com>
2025-01-24 01:27:09 +0600

    Initial commit

---
+++ src/components/Nested.svelte
@@ -0,0 +1,41 @@
+<script>
+	let count = $state(0);
+
+	let numbers = $state([1, 2, 3, 4, 5]);
+
+	let total = $derived(numbers.reduce((a, b) => a + b, 0));
+
+	function increment() {
+		count++;
+	}
+
+	function addNumber() {
+		numbers.push(numbers.length);
+	}
+</script>
+
+<p>This is a nested component</p>
+
+<button onclick={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<button onclick={addNumber}>
+	{numbers.join(' + ')} = {total}
+</button>
+
+<style>
+    p {
+        color: goldenrod;
+        font-family: 'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
+
+    button {
+        color: goldenrod;
+        font-size: 1.2rem;
+        padding: 0.5rem 1rem;
+        border: 1px solid goldenrod;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+	import Nested from "../components/Nested.svelte";
+
+	let name = "asif";
+	let src = 'https://placehold.co/600x400'
+</script>
+
+<h1>Welcome to Svelte, {name}</h1>
+<h2>Manipulation: {name.toUpperCase()}</h2>
+
+<br>
+
+<img {src} alt="{name} dances"/>
+
+<Nested />
+
